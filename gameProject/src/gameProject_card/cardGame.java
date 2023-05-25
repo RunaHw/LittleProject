5c595acfc06705136c9afe6696573a0c32da6a13
RunaHw <103980790+RunaHw@users.noreply.github.com>
2023-05-25 09:51:17 +0900

    Add files via upload

--- gameProject/src/gameProject_card/cardGame.java
+++ gameProject/src/gameProject_card/cardGame.java
@@ -25,6 +25,7 @@ public class cardGame {
 			}
 
 			else if (start == 2) {
+				System.out.println("-------------------------------------------------------------------");
 				System.out.println("결과 >> 승 : " + winCount + ", 패 : " + loseCount);
 				System.out.println("-------------------------------------------------------------------");
 				continue;
@@ -33,7 +34,10 @@ public class cardGame {
 			else if (start == 3)
 				break;
 			else {
+				System.out.println("-------------------------------------------------------------------");
 				System.out.println("잘 못입력했습니다.");
+				System.out.println("1~3 사이의 숫자를 다시 입력해 주세요.");
+				System.out.println("-------------------------------------------------------------------");
 				continue;
 			}
 
@@ -47,21 +51,26 @@ public class cardGame {
 			System.out.println("-------------------------------------------------------------------");
 			System.out.println("당신의 카드 >> card1 : " + card1 + ", card2 : " + card2);
 			System.out.println("당신의 카드 조합 >> " + bigger);
+			System.out.println("-------------------------------------------------------------------");
 			System.out.println();
 			System.out.println("1. 이긴다.  2. 진다. 3. SIMILAR>>(+-5차이)");
 			System.out.print(">>> ");
 			int select = sc.nextInt();
+			System.out.println("-------------------------------------------------------------------");
 			int batting = 0;
 			int lastMoney = 0;
 
 			while (true) {
+				System.out.println("-------------------------------------------------------------------");
 				System.out.println("배팅 금액을 적어주세요.");
 				System.out.println("Your money = " + money);
 				System.out.print(">>> ");
 				batting = sc.nextInt();
+				System.out.println("-------------------------------------------------------------------");
 				lastMoney = money;
 				money -= batting;
 				if (money < 0) {
+					System.out.println("소지금이 부족합니다.");
 					money = lastMoney;
 					continue;
 
@@ -135,13 +144,17 @@ public class cardGame {
 			System.out.println("-------------------------------------------------------------------");
 			System.out.println();
 			if (money <= 0) {
+				System.out.println("풉ㅋ풉ㅋ 돈이 떨어졌네요~");
 				break;
 
 			}
 			// while
 		}
-		System.out.println("감사합니다.");
+		System.out.println("-------------------------------------------------------------------");
+		System.out.println("게임을 종료합니다.");
 		System.out.println("결과 >> 승 : " + winCount + ", 패 : " + loseCount);
+		System.out.println("Your money = " + money);
+		System.out.println("-------------------------------------------------------------------");
 		sc.close();
 	}
 }
